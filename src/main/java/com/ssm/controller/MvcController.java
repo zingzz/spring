7c58de05ad893d012703bb3237e932a0b7051cc9
243653602@qq.com <243653602@qq.com>
2018-04-15 10:04:19 +0800

    add project

---
+++ src/main/java/com/ssm/controller/MvcController.java
@@ -0,0 +1,30 @@
+package com.ssm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONObject;
+
+@Controller
+@RequestMapping("/test")
+public class MvcController {
+	@RequestMapping("tojson.json")
+	public JSONObject testJson(HttpServletRequest request, HttpServletResponse response) {
+		JSONObject result = new JSONObject();
+		result.put("hehe", "1111");
+		return result;
+	}
+	
+	@RequestMapping("tojson.html")
+	public ModelAndView testHtml(HttpServletRequest request, HttpServletResponse response,ModelAndView mav) {
+
+		mav.addObject("what", "the best");
+		mav.setViewName("666");
+		return mav;
+	}
+
+}
---
+++ src/main/java/com/ssm/view/MappingJacksonJsonView.java
@@ -0,0 +1,15 @@
+package com.ssm.view;
+
+import java.util.Map;
+
+public class MappingJacksonJsonView extends org.springframework.web.servlet.view.json.MappingJackson2JsonView  {
+	@Override
+	protected Object filterModel(Map<String, Object> model) {
+		Map<?, ?> result = (Map<?, ?>) super.filterModel(model);    
+	    if (result.size() == 1) {    
+	        return result.values().iterator().next();    
+	    } else {    
+	        return result;    
+	    }    
+	}
+}
