076fcefe62d5f75a368cd254b32be8d9d76f39b3
243653602@qq.com <243653602@qq.com>
2018-05-17 15:39:10 +0800

    升级spring mybatis版本      crud测试

--- src/main/java/com/ssm/controller/MvcController.java
+++ src/main/java/com/ssm/controller/MvcController.java
@@ -3,27 +3,31 @@ package com.ssm.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ssm.dao.UserMapper;
 
 @Controller
 @RequestMapping("/test")
 public class MvcController {
+	@Autowired
+	private UserMapper userMapper;
 	@RequestMapping("tojson.json")
 	public JSONObject testJson(HttpServletRequest request, HttpServletResponse response) {
 		JSONObject result = new JSONObject();
-		result.put("hehe", "1111");
+		result.put("user", userMapper.selectByPrimaryKey(1L));
 		return result;
 	}
 	
 	@RequestMapping("tojson.html")
 	public ModelAndView testHtml(HttpServletRequest request, HttpServletResponse response,ModelAndView mav) {
 
-		mav.addObject("what", "the best");
-		mav.setViewName("666666");
+		mav.addObject("what", "qqqqq");
+		mav.setViewName("666");
 		return mav;
 	}
 
